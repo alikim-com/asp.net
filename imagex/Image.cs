ff4cae33b1916523513a762687cdc88cd9bba439
alikim <alikims@gmail.com>
2024-02-08 02:24:56 +1100

    imagex : zero dependency cross platform minimal image library - initial commit

---
+++ imagex/Image.cs
@@ -0,0 +1,20 @@
+﻿
+namespace imagex;
+
+public enum Format
+{
+    JPEG,
+    PNG,
+}
+
+public class Image(Format _format, int _width, int _height)
+{
+    public readonly Format format = _format;
+    public readonly int width = _width;
+    public readonly int height = _height;
+}
+
+public class PNG(Format _format, int _width, int _height) : Image(_format, _width, _height)
+{
+    
+}
